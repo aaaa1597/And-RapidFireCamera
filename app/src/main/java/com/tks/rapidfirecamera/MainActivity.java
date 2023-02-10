45a319972db22d1fb570f84fb3273e5ca7bb9f87
aaaaa <aaaaa>
2023-02-10 21:37:18 +0900

    UIのひな型作成

--- app/src/main/java/com/tks/rapidfirecamera/MainActivity.java
+++ app/src/main/java/com/tks/rapidfirecamera/MainActivity.java
@@ -1,10 +1,7 @@
 package com.tks.rapidfirecamera;
 
-import androidx.activity.result.ActivityResultLauncher;
-import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.Manifest;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
@@ -13,26 +10,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        /* get camera permission */
-        ActivityResultLauncher<String> launcher
-                = registerForActivityResult(new ActivityResultContracts.RequestPermission(),
-                (isGranted) -> {
-                    if (isGranted) {
-                        /* 権限取得 OK時 -> Fragment追加 */
-                        if (null == savedInstanceState) {
-                            getSupportFragmentManager().beginTransaction()
-                                    .replace(R.id.container, MainFragment.newInstance())
-                                    .commit();
-                        }
-                    }
-                    else {
-                        /* 権限取得 拒否時 -> ErrorダイアグOpenでアプリ終了!! */
-                        MainFragment.ErrorDialog.newInstance(getString(R.string.request_permission)).show(getSupportFragmentManager(), "Error!!");
-                    }
-                });
-
-        /* request camera permission */
-        launcher.launch(Manifest.permission.CAMERA);
+        if (null == savedInstanceState) {
+            getSupportFragmentManager().beginTransaction()
+                    .replace(R.id.container, MainFragment.newInstance())
+                    .commit();
+        }
     }
 }

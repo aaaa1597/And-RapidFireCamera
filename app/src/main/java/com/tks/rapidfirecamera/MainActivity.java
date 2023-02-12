63ba18f9f0598e8e73136ac8bdbff7ed1e042792
aaaaa <aaaaa>
2023-02-12 10:17:18 +0900

    カメラPermission取得処理追加

--- app/src/main/java/com/tks/rapidfirecamera/MainActivity.java
+++ app/src/main/java/com/tks/rapidfirecamera/MainActivity.java
@@ -1,6 +1,9 @@
 package com.tks.rapidfirecamera;
 
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.WindowCompat;
 
 import android.os.Bundle;
 
@@ -10,10 +13,30 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        if (null == savedInstanceState) {
-            getSupportFragmentManager().beginTransaction()
-                    .replace(R.id.container, MainFragment.newInstance())
-                    .commit();
-        }
+
+        WindowCompat.setDecorFitsSystemWindows(getWindow(), false);
+        getWindow().setStatusBarContrastEnforced(false);
+        getWindow().setNavigationBarContrastEnforced(false);
+
+        /* get camera permission */
+        ActivityResultLauncher<String> launcher
+                = registerForActivityResult(new ActivityResultContracts.RequestPermission(),
+                (isGranted) -> {
+                    if (isGranted) {
+                        /* 権限取得 OK時 -> Fragment追加 */
+                        if (null == savedInstanceState) {
+                            getSupportFragmentManager().beginTransaction()
+                                    .replace(R.id.container, MainFragment.newInstance())
+                                    .commit();
+                        }
+                    }
+                    else {
+                        /* 権限取得 拒否時 -> ErrorダイアグOpenでアプリ終了!! */
+                        MainFragment.ErrorDialog.newInstance(getString(R.string.request_permission)).show(getSupportFragmentManager(), "Error!!");
+                    }
+                });
+
+        /* request camera permission */
+        launcher.launch(android.Manifest.permission.CAMERA);
     }
 }
--- app/src/main/java/com/tks/rapidfirecamera/MainFragment.java
+++ app/src/main/java/com/tks/rapidfirecamera/MainFragment.java
@@ -1,23 +1,39 @@
 package com.tks.rapidfirecamera;
 
+import androidx.annotation.RequiresApi;
+import androidx.appcompat.app.AlertDialog;
+import androidx.fragment.app.DialogFragment;
 import androidx.lifecycle.ViewModelProvider;
 
+import android.app.Activity;
+import android.app.Dialog;
+import android.content.DialogInterface;
+import android.graphics.Insets;
+import android.os.Build;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import android.os.Handler;
+import android.os.HandlerThread;
 import android.util.SparseIntArray;
 import android.view.LayoutInflater;
 import android.view.Surface;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.WindowInsets;
+import android.view.WindowManager;
+import android.view.WindowMetrics;
 
 public class MainFragment extends Fragment {
     private MainViewModel mViewModel;
+    private AutoFitTextureView mTextureView;
+    private String mCameraId;
+    private HandlerThread mBackgroundThread;
+    private Handler mBackgroundHandler;
     private static final SparseIntArray ORIENTATIONS = new SparseIntArray();
-    private static final int REQUEST_CAMERA_PERMISSION = 1;
     static {
         ORIENTATIONS.append(Surface.ROTATION_0, 90);
         ORIENTATIONS.append(Surface.ROTATION_90, 0);
@@ -30,8 +46,7 @@ public class MainFragment extends Fragment {
     }
 
     @Override
-    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
-                             @Nullable Bundle savedInstanceState) {
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_main, container, false);
     }
 
@@ -41,4 +56,35 @@ public class MainFragment extends Fragment {
         mViewModel = new ViewModelProvider(this).get(MainViewModel.class);
         // TODO: Use the ViewModel
     }
+
+    public static class ErrorDialog extends DialogFragment {
+        private static final String ARG_MESSAGE = "message";
+        public static ErrorDialog newInstance(String message) {
+            ErrorDialog dialog = new ErrorDialog();
+            Bundle args = new Bundle();
+            args.putString(ARG_MESSAGE, message);
+            dialog.setArguments(args);
+            return dialog;
+        }
+
+        @NonNull
+        @Override
+        public Dialog onCreateDialog(@Nullable Bundle savedInstanceState) {
+            final Activity activity = getActivity();
+            if(activity == null) throw new RuntimeException("illegal state!! activity is null!!");
+            android.os.Bundle bundle = getArguments();
+            if(bundle == null) throw new RuntimeException("illegal state!! bundle is null!!");
+
+            return new AlertDialog.Builder(activity)
+                    .setMessage(bundle.getString(ARG_MESSAGE))
+                    .setPositiveButton(android.R.string.ok, new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialogInterface, int i) {
+                            activity.finish();
+                        }
+                    })
+                    .create();
+        }
+    }
+
 }
